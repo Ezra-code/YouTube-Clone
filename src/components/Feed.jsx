43110b1cb91ab672eff9659bee6e3b9f7c9fb2f0
Ezra <njerukithinji872@gmail.com>
2022-12-28 18:37:22 +0300

    added apis

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import SideBar from "./SideBar";
+import Videos from "./Videos";
+
+import { fetchFromApi } from "../utils/fetchFromApi";
+
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -9,6 +13,15 @@ const Feed = () => {
         <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
           copyright 2022
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{
+          color: 'white'
+        }}>
+          New<span style={{color: '#F31503'}}>Videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions'
+const options = {
+    params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+  };
+
+  export const fetchFromApi = async(url) => {
+    const {data} = await axios.get(`${BASE_URL}/${url}`, options)
+    return data
+  }
